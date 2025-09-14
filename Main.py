9f11d3a45bad24ebdad5a3c7228dcb9f318300fe
CarlosPurcaru <carlospurcaru@gmail.com>
2025-09-14 17:49:58 -0300

    Branch1

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 #Criação da Main
 
 # Bloco 1 - Entrada de dados
-numero1 = int(input("(alterado)Digite o primeiro número: "))
-numero2 = int(input("(alterado)Digite o segundo número: "))
+numero1 = int(input("(branch1)Digite o primeiro número: "))
+numero2 = int(input("(branch1)Digite o segundo número: "))
