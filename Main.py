4f12174df33ae53f117e9cddee8b4bdce70b4ee2
CarlosPurcaru <carlospurcaru@gmail.com>
2025-09-14 17:54:52 -0300

    Branch2

--- Main.py
+++ Main.py
@@ -3,3 +3,10 @@
 # Bloco 1 - Entrada de dados
 numero1 = int(input("(alterado)Digite o primeiro número: "))
 numero2 = int(input("(alterado)Digite o segundo número: "))
+
+# Bloco 2 - Processamento (cálculo da soma)
+soma = numero1 + numero2
+if numero2 != 0:
+    divisao = numero1 / numero2
+else:
+    divisao = "Indefinida (divisão por zero)"
