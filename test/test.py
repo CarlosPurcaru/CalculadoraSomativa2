0052d2367dcc67b7c861377e2fd5084cee352d76
CarlosPurcaru <carlospurcaru@gmail.com>
2025-09-21 13:13:39 -0300

    test_2

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
-from src.Main import calcular_soma, calcular_divisao
+from src.Main import calcular_soma, calcular_divisao, calcular_subtracao
+
 
 def test_calcular_soma():
     assert calcular_soma(3, 5) == 8
@@ -8,3 +9,7 @@ def test_calcular_divisao():
     assert calcular_divisao(10, 2) == 5
     assert calcular_divisao(9, 3) == 3
     assert calcular_divisao(5, 0) == "Indefinida (divisão por zero)"
+
+def test_calcular_subtracao():
+    assert calcular_subtracao(10, 5) == 5
+    assert calcular_subtracao(5, 10) == -5
