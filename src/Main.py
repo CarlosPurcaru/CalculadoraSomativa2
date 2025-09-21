537636494fc9d9e255de9f2baac105377a6eb134
CarlosPurcaru <carlospurcaru@gmail.com>
2025-09-21 13:31:36 -0300

    test_3

--- src/Main.py
+++ src/Main.py
@@ -8,6 +8,13 @@ def calcular_divisao(numero1: int, numero2: int):
         return numero1 / numero2
     return "Indefinida (divisão por zero)"
 
+def calcular_subtracao(numero1: int, numero2: int) -> int:
+    return numero1 - numero2
+
+def calcular_multiplicacao(numero1: int, numero2: int) -> int:
+    return numero1 * numero2
+
+
 # Bloco 1
 def executar():
     numero1 = int(input("(alterado)Digite o primeiro número: "))
@@ -16,10 +23,14 @@ def executar():
 # Bloco 2
     soma = calcular_soma(numero1, numero2)
     divisao = calcular_divisao(numero1, numero2)
+    subtracao = calcular_subtracao(numero1, numero2)
+    multiplicacao = calcular_multiplicacao(numero1, numero2)
 
 # Bloco 3
     print("A soma é:", soma)
     print("A divisão é:", divisao)
+    print("A subtração é:", subtracao)
+    print("A multiplicação é:", multiplicacao)
 
 if __name__ == "_main_":
     executar()
--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from src.Main import calcular_soma, calcular_divisao
+from src.Main import calcular_soma, calcular_divisao, calcular_subtracao, calcular_multiplicacao
 
 def test_calcular_soma():
     assert calcular_soma(3, 5) == 8
@@ -8,3 +8,11 @@ def test_calcular_divisao():
     assert calcular_divisao(10, 2) == 5
     assert calcular_divisao(9, 3) == 3
     assert calcular_divisao(5, 0) == "Indefinida (divisão por zero)"
+
+def test_calcular_subtracao():
+    assert calcular_subtracao(10, 5) == 5
+    assert calcular_subtracao(5, 10) == -5
+
+def test_calcular_multiplicacao():
+    assert calcular_multiplicacao(3, 4) == 12
+    assert calcular_multiplicacao(0, 99) == 0
